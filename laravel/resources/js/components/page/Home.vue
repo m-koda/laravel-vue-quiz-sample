0fceae90d78eb87f4b9abd9d8f77203f57e0739b
m-koda <makotokoda82@gmail.com>
2020-06-11 21:18:00 +0900

    カテゴリー取得APIの実装

--- laravel/resources/js/components/page/Home.vue
+++ laravel/resources/js/components/page/Home.vue
@@ -17,23 +17,9 @@
               <img class="home-quiz__setting-h2-logo" src="/images/directory-icon.png" />出題設定
             </h2>
             <form>
-              <label>
-                <input type="checkbox" v-model="categories" value="1" checked />ビジネスマナー
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="2" />一般常識
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="3" />就職・転職
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="4" />法律
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="5" />IT
-              </label>
-              <label>
-                <input type="checkbox" v-model="categories" value="6" />雑学
+              <label v-for="(cate, index) in category" v-bind:key="index">
+                <input type="checkbox" v-model="categories" v-bind:value="cate.id" />
+                {{ cate.name }}
               </label>
               <div class>
                 全項目チェック
@@ -98,12 +84,18 @@ export default {
   data() {
     return {
       categories: [1],
-      informations: []
+      informations: [],
+      category: []
     };
   },
   mounted() {
+    // お知らせの取得
     this.$http.get("/api/information").then(response => {
       this.informations = response.data;
+    });
+    // カテゴリーの取得
+    this.$http.get("/api/category").then(response => {
+      this.category = response.data;
     });
   },
   methods: {
