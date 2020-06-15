442fc596cf344f631e54bd5ecaec2cc1db08f678
m-koda <makotokoda82@gmail.com>
2020-06-15 22:04:03 +0900

    APIに認証機能を追加

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -9,6 +9,7 @@ window.$ = window.jQuery = jQuery;
 require('bootstrap');
 
 Vue.prototype.$http = axios;
+axios.defaults.headers.common['Authorization'] = "Bearer " + document.querySelector('meta[name="api-token"]').getAttribute("content");
 
 Vue.use(SocialSharing);
 
--- laravel/resources/js/components/page/MyPage.vue
+++ laravel/resources/js/components/page/MyPage.vue
@@ -23,6 +23,11 @@ import TheSidebar from "../layout/TheSidebar";
 export default {
   components: {
     TheSidebar
+  },
+  mounted() {
+    this.$http.get("/api/user").then(response => {
+      console.log(response.data);
+    });
   }
 };
 </script>
